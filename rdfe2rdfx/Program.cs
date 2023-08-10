67e4ea756e25299e5c4bf0f735228652e8bbcc6b
nil4 <nil4@users.noreply.github.com>
2023-08-10 13:27:04 +0200

    Conversion tool (`.rdfe` -> `.rdfx`)
    Simple .NET tool to convert `.rdfe` (JSON format) files to `.rdfx` (XML format).
    
    Can convert either individual files, or all files anywhere under a root folder.
    
    The conversion is side-by-side; an `.rdfx` file is dropped next to each input `.rdfe`.

---
+++ rdfe2rdfx/Program.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Xml;
+using static System.Console;
+
+static class Program
+{
+    const string InputExtension = ".rdfe";
+    const string OutputExtension = ".rdfx";
+
+    // exit codes
+    const int StatusSuccess = 0;
+    const int StatusUsageError = 1;
+    const int StatusConversionError = 2;
+    
+    static int Main(string[] args)
+    {
+        if (args.Length != 1) 
+            return UsageError($"Required <file{InputExtension}> or <directory> path not specified");
+
+        var path = args[0];
+        if (!Path.Exists(path)) 
+            return UsageError($"Specified path '{path}' not found");
+        
+        if (File.Exists(path) && Path.GetExtension(path) is not InputExtension)
+            return UsageError($"Input file '{path}' must have {InputExtension} extension");
+
+        try
+        {
+            if (File.Exists(path)) ConvertJsonFile(path);
+            else ConvertDirectoryFiles(path);
+            
+            return StatusSuccess;
+        }
+        catch (Exception ex)
+        {
+            Error.WriteLine($"Error: {ex}");
+            return StatusConversionError;
+        }
+    }
+
+    static void ConvertJsonFile(string path)
+    {
+        WriteLine($"Reading file '{path}'");
+        
+        using FileStream stream = File.OpenRead(path);
+        var data = JsonSerializer.Deserialize<DynamicFolderExport>(stream, new JsonSerializerOptions
+        {
+            IncludeFields = false,
+            MaxDepth = 8,
+            AllowTrailingCommas = true,
+            ReadCommentHandling = JsonCommentHandling.Skip,
+            UnmappedMemberHandling = JsonUnmappedMemberHandling.Disallow,
+        });
+        Debug.Assert(data is not null);
+
+        string outputPath = Path.ChangeExtension(path, OutputExtension);
+
+        WriteLine($"Writing data to {outputPath}");
+        ConvertDataToXml(data, outputPath);
+    }
+
+    static void ConvertDataToXml(DynamicFolderExport data, string path)
+    {
+        using FileStream stream = File.OpenWrite(path);
+        using var xml = XmlWriter.Create(stream, new XmlWriterSettings
+        {
+            Encoding = Encoding.UTF8,
+            Indent = true,
+            IndentChars = "    ",
+            CloseOutput = true,
+            ConformanceLevel = ConformanceLevel.Fragment,
+            NewLineHandling = NewLineHandling.None,
+            OmitXmlDeclaration = true,
+            WriteEndDocumentOnClose = true,
+        });
+        
+        xml.WriteStartElement(nameof(DynamicFolderExport));
+        WriteStringValue(xml, nameof(DynamicFolderExport.Name), data.Name);
+        
+        xml.WriteStartElement(nameof(DynamicFolderExport.Objects));
+        foreach (var entry in data.Objects)
+        {
+            xml.WriteStartElement(nameof(DynamicFolderExportObject));
+            WriteStringValue(xml, nameof(DynamicFolderExportObject.Type), entry.Type);
+            WriteStringValue(xml, nameof(DynamicFolderExportObject.Name), entry.Name);
+            WriteStringValue(xml, nameof(DynamicFolderExportObject.Description), entry.Description);
+            WriteStringValue(xml, nameof(DynamicFolderExportObject.Notes), entry.Notes);
+
+            xml.WriteStartElement(nameof(DynamicFolderExportObject.CustomProperties));
+            foreach (var prop in entry.CustomProperties)
+            {
+                xml.WriteStartElement(nameof(CustomProperty));
+                WriteStringValue(xml, nameof(prop.Name), prop.Name);
+                WriteStringValue(xml, nameof(prop.Type), prop.Type);
+                WriteStringValue(xml, nameof(prop.Value), prop.Value);
+                xml.WriteEndElement(/* nameof(CustomProperty) */);
+            }
+            xml.WriteEndElement(/* nameof(DynamicFolderExportObject.CustomProperties)*/ );
+            
+            WriteStringValue(xml, nameof(DynamicFolderExportObject.ScriptInterpreter), entry.ScriptInterpreter);
+            WriteStringValue(xml, nameof(DynamicFolderExportObject.Script), entry.Script);
+
+            WriteStringValue(xml, nameof(DynamicFolderExportObject.DynamicCredentialScriptInterpreter), entry.DynamicCredentialScriptInterpreter);
+            WriteStringValue(xml, nameof(DynamicFolderExportObject.DynamicCredentialScript), entry.DynamicCredentialScript);
+            
+            xml.WriteEndElement(/* nameof(DynamicFolderExportObject) */);
+        }
+        
+        xml.WriteEndElement(/* nameof(DynamicFolderExport.Objects) */);
+        xml.WriteEndElement(/* nameof(DynamicFolderExport) */);
+        
+        xml.Flush();
+        stream.Flush();
+    }
+
+    static void WriteStringValue(XmlWriter writer, string elementName, string? value)
+    {
+        if (HasMultipleNewlines(value))
+        {
+            writer.WriteStartElement(elementName);
+            writer.WriteCData(value);
+            writer.WriteEndElement();
+        }
+        else writer.WriteElementString(elementName, value);
+
+        static bool HasMultipleNewlines(string? s)
+        {
+            if (string.IsNullOrEmpty(s)) return false;
+            var p = s.IndexOf('\n');
+            if (p < 0) return false;
+            p = s.IndexOf('\n', p);
+            return p > 0;
+        }
+    }
+
+    static void ConvertDirectoryFiles(string path)
+    {
+        foreach (var file in Directory.EnumerateFiles(path, "*" + InputExtension, SearchOption.AllDirectories))
+            ConvertJsonFile(file);
+    }
+    
+    static int UsageError(string message)
+    {
+        string programName = Path.GetFileNameWithoutExtension(Assembly.GetEntryAssembly()!.Location);
+       
+        Error.WriteLine(message);
+        Error.WriteLine();
+        Error.WriteLine($"Usage: {programName} <file{InputExtension} | directory>");
+        Error.WriteLine($"   file{InputExtension}: write the equivalent {OutputExtension} side-by-side");
+        Error.WriteLine($"   directory: convert all {InputExtension} files and write {OutputExtension} side-by-side");
+        return StatusUsageError;
+    }
+}
+
+// ReSharper disable ClassNeverInstantiated.Global
+// ReSharper disable UnusedMember.Global
+// ReSharper disable CollectionNeverUpdated.Global
+sealed class DynamicFolderExport
+{
+    public string? Name { get; set; }
+    public List<DynamicFolderExportObject> Objects { get; set; } = new();
+}
+
+sealed class DynamicFolderExportObject
+{
+    public string? Type { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public string? Notes { get; set; }
+    public List<CustomProperty> CustomProperties { get; set; } = new();
+    public string? Script { get; set; }
+    public string? ScriptInterpreter { get; set; }
+    public string? DynamicCredentialScriptInterpreter { get; set; }
+    public string? DynamicCredentialScript { get; set; }
+}
+
+sealed class CustomProperty
+{
+    public string? Name { get; set; }
+    public string? Type { get; set; }
+    public string? Value { get; set; }
+}
